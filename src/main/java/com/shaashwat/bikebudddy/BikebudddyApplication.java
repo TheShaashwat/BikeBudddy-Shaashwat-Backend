97bac6c9ec90957ec72b9b36924083575126e1fa
TheShaashwat <theshaashwat@gmail.com>
2026-01-03 20:30:57 +0530

    committing first project initialization

---
+++ src/main/java/com/shaashwat/bikebudddy/BikebudddyApplication.java
@@ -0,0 +1,13 @@
+package com.shaashwat.bikebudddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikebudddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikebudddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shaashwat/bikebudddy/BikebudddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaashwat.bikebudddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikebudddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
